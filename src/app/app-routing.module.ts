9ad907af81736faf1d42145394ac707016d46ae0
farith <oikfarith@gmail.com>
2022-05-22 13:20:18 +0530

    First

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RxjsLearningComponent } from './rxjs-learning/rxjs-learning.component';
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+  Routes = [
+    //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   // { path: 'dashboard', component: DashboardComponent },
+    //{ path: 'detail/:id', component: HeroDetailComponent },
+    //{ path: 'heroes', component: HeroesComponent },
+    { path: 'rxjs', component: RxjsLearningComponent }
+  ];
+ 
+  
+ }
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import{Observable,of,map,first, animationFrames, takeWhile, endWith } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  studentList = ['mark','ram','sita','lisa'];
+students : Observable<string[]> = of(this.studentList);
+
+
+
+
+ngOnInit(): void {
+  this.students.subscribe(data => {
+
+console.log(data);
+
+
+of(1, 2, 3)
+  .pipe(map((x) => x * x))
+  .subscribe((v) => console.log(`value: ${v}`));
+
+  of(1, 2, 3)
+  .pipe(first())
+  .subscribe((v) => console.log(`value: ${v}`));
+
+  })
+}
+
+
+
+}
+
+
+
